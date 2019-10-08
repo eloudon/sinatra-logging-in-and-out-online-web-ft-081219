5e096b56be3dc7112d2285e15961674562ec8152
Eric Loudon <loudon.eric@gmail.com>
2019-10-08 10:22:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
       redirect to '/account'
     else
       erb :error
-   end
+    end
   end
 
   get '/account' do
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
     erb :account
   else
     erb :error
-   end
+    end
   end
 
   get '/logout' do
